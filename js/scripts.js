fc011017c03ac22d2052b7c66613004ee280892e
agatakolohe <agatakolohe@gmail.com>
2020-09-05 18:29:29 -0700

    add bootstrap, jquery, buttons for dark and blue mode, add js methods to change the color of background when clicked on button

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("button#black").click(function() {
+    $("body").css("background-color", "black");
+  });
+  $("button#blue").click(function() {
+    $("body").css("background-color", "mediumturquoise");
+  });
+});
